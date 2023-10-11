e0ae9c0a4501cb42ef7623d25d2039a141053cfc
BaptisteBetclic <130462018+BaptisteBetclic@users.noreply.github.com>
2023-10-11 14:02:20 +0200

    Init participant-list

--- apps/spin-the-team/src/app/app.routes.ts
+++ apps/spin-the-team/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import {HomeTestComponent} from "@spin-the-team/libs/participant-list";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path:'participant-list',
+    component: HomeTestComponent
+  }
+];
---
+++ libs/participant-list/src/lib/home-test/home-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'spin-the-team-home-test',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-test.component.html',
+  styleUrls: ['./home-test.component.scss'],
+})
+export class HomeTestComponent {}
