30595de6bd99802c3f4ce1e5a3352db3ed4fdf5c
Andrei Serguienko <serguienko.a@gmail.com>
2023-10-11 13:46:21 +0200

    Init firebase

--- apps/spin-the-team/src/app/app.component.ts
+++ apps/spin-the-team/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { initializeApp } from "firebase/app";
+
 
 @Component({
   standalone: true,
@@ -8,6 +10,18 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'spin-the-team';
+export class AppComponent implements OnInit {
+
+  public ngOnInit() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyB6HWXc80FSjt7dv0NntWb2_z8wSGehDac",
+      authDomain: "spin-the-team.firebaseapp.com",
+      projectId: "spin-the-team",
+      storageBucket: "spin-the-team.appspot.com",
+      messagingSenderId: "90034367233",
+      appId: "1:90034367233:web:f6149b27c61ad2d1654900"
+    };
+
+    initializeApp(firebaseConfig);
+  }
 }
