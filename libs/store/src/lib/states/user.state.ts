ec5848cf245ee2261f48fd1c3378fb3571f2eb60
Andrei Serguienko <serguienko.a@gmail.com>
2023-10-12 11:17:17 +0200

    User store

--- libs/store/src/lib/states/user.state.ts
+++ libs/store/src/lib/states/user.state.ts
@@ -1,7 +1,9 @@
 import { User } from '@spin-the-team/models';
 import { Action, State, StateContext } from '@ngxs/store';
-import { Injectable } from '@angular/core';
-import { CreateUser } from '../actions/user.action';
+import { inject, Injectable } from '@angular/core';
+import { CreateUser, GetUsers } from '../actions/user.action';
+import { UserService } from '@spin-the-team/firebase-helper';
+import { tap } from 'rxjs';
 
 export interface UserStateModel {
   listUsers: User[];
@@ -15,12 +17,28 @@ export interface UserStateModel {
 })
 @Injectable()
 export class UserState {
+  private readonly userService = inject(UserService);
   @Action(CreateUser)
   public createUser(ctx: StateContext<UserStateModel>, action: CreateUser) {
+    this.userService.createUser(action.payload);
+
     const state = ctx.getState();
     ctx.setState({
       ...state,
       listUsers: [...state.listUsers, action.payload]
     });
+  }
+
+  @Action(GetUsers)
+  public getUsers(ctx: StateContext<UserStateModel>) {
+    this.userService.userList()
+      .pipe(
+        tap((users: User[]) => {
+          ctx.setState({
+            listUsers: users
+          });
+        }
+      ))
+      .subscribe();
   }
 }
