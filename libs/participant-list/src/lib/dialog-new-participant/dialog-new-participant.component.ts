0fa2728588bb183078c76fb7ce4eed1f3aa7da10
BaptisteBetclic <130462018+BaptisteBetclic@users.noreply.github.com>
2023-10-12 15:18:51 +0200

    Style css for container

--- libs/participant-list/src/lib/dialog-new-participant/dialog-new-participant.component.ts
+++ libs/participant-list/src/lib/dialog-new-participant/dialog-new-participant.component.ts
@@ -1,8 +1,9 @@
-import {Component, Inject} from '@angular/core';
+import {Component, inject, Inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 
 export interface DialogData {
   name: string;
@@ -12,11 +13,14 @@ export interface DialogData {
 @Component({
   selector: 'spin-the-team-dialog-new-participant',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatInputModule, FormsModule, MatButtonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatInputModule, FormsModule, MatButtonModule, MatDialogModule],
   templateUrl: './dialog-new-participant.component.html',
   styleUrls: ['./dialog-new-participant.component.scss'],
 })
 export class DialogNewParticipantComponent {
+
+  private readonly dialogRef = inject(MatDialogRef)
+
   public form = new FormGroup({
     name: new FormControl(null, Validators.required),
     img_url: new FormControl(null, Validators.required),
@@ -28,5 +32,10 @@ export class DialogNewParticipantComponent {
 
   public get img_url(): FormControl {
     return this.form.get('img_url') as FormControl
+  }
+
+  public submit(): void {
+    const formValue = this.form.value;
+    return this.dialogRef.close(formValue);
   }
 }
