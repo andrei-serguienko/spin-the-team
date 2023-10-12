009e7b1bb7b7455b8797d7c2117f972ed8d0b356
BaptisteBetclic <130462018+BaptisteBetclic@users.noreply.github.com>
2023-10-12 09:00:33 +0200

    Changes by a.serguienko

--- libs/participant-list/src/lib/participant-list-container/participant-list-container.component.ts
+++ libs/participant-list/src/lib/participant-list-container/participant-list-container.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {ParticipantCardComponent} from "../participant-card/participant-card.component";
+import {Component, inject} from '@angular/core';
+import {CommonModule, NgFor} from '@angular/common';
+import {CurrentParticipantCardComponent} from "../current-participant-card/current-participant-card.component";
+import {UserService} from "@spin-the-team/firebase-helper";
+import {ParicpantCardComponent} from "../paricpant-card/paricpant-card.component";
 
 @Component({
   selector: 'spin-the-team-participant-list-container',
   standalone: true,
-  imports: [CommonModule, ParticipantCardComponent],
+  imports: [CommonModule, CurrentParticipantCardComponent, NgFor, ParicpantCardComponent],
   templateUrl: './participant-list-container.component.html',
   styleUrls: ['./participant-list-container.component.scss'],
 })
-export class ParticipantListContainerComponent {}
+export class ParticipantListContainerComponent {
+  private readonly userService = inject(UserService);
+
+  protected readonly users$ = this.userService.userList()
+}
